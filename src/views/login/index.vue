2953cfa94a79c7e7493450bb4ae5409702015c84
yuki <90huanw@gmail.com>
2020-10-18 15:21:07 +0800

    修改成功

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -57,14 +57,14 @@ export default {
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请输入正确的用户名！'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+      if (value.length < 1) {
+        callback(new Error('请输入合法的密码！'))
       } else {
         callback()
       }
